31bb4fd32500c3593b919de8890d852712724aea
Coleman <omegacoleman@gmail.com>
2024-06-04 05:21:53 +0800

    support and default to unix socket (#3)
    Co-authored-by: Qijia Liu <liumeo@pku.edu.cn>

--- test/testbeast.cpp
+++ test/testbeast.cpp
@@ -39,28 +39,56 @@ int main() {
         return "";
     });
 
-    // Listen on default port.
-    assert(std::system("lsof -i:32489") == 0);
+    // Listen on default unix socket.
+    assert(std::system("lsof /tmp/fcitx5.sock") == 0);
 
     // Getter works.
     assert(!getterCalled);
-    assert(std::system("curl http://localhost:32489/config/addon/beast") == 0);
+    assert(std::system("curl --unix-socket /tmp/fcitx5.sock "
+                       "http://fcitx/config/addon/beast") == 0);
     assert(getterCalled);
 
     // Setter works.
     assert(!setterCalled);
-    assert(
-        std::system(
-            "curl -X POST -d '{}' http://localhost:32489/config/addon/beast") ==
-        0);
+    assert(std::system("curl --unix-socket /tmp/fcitx5.sock -X POST -d '{}' "
+                       "http://fcitx/config/addon/beast") == 0);
     assert(setterCalled);
 
-    // Port reset works.
+    // Unix socket path reset works.
     auto config = dynamic_cast<const BeastConfig *>(beast->getConfig());
     RawConfig raw;
     config->dumpDescription(raw);
     BeastConfig cfg;
-    cfg.port.setValue(32490);
+    cfg.unix_socket.mutableValue()->path.setValue("/tmp/fcitx5-another.sock");
+    cfg.save(raw);
+    beast->setConfig(raw);
+
+    assert(std::system("lsof /tmp/fcitx5.sock") != 0);
+    assert(std::system("lsof /tmp/fcitx5-another.sock") == 0);
+
+    // Tcp default port works.
+    cfg.communication.setValue(BeastCommunication::TCP);
+    cfg.save(raw);
+    beast->setConfig(raw);
+    assert(std::system("lsof -i:32489") == 0);
+
+    getterCalled = false;
+    setterCalled = false;
+
+    // Getter works over TCP.
+    assert(!getterCalled);
+    assert(std::system("curl "
+                       "http://127.0.0.1:32489/config/addon/beast") == 0);
+    assert(getterCalled);
+
+    // Setter works ovet TCP.
+    assert(!setterCalled);
+    assert(std::system("curl -X POST -d '{}' "
+                       "http://127.0.0.1:32489/config/addon/beast") == 0);
+    assert(setterCalled);
+
+    // Tcp port reset works.
+    cfg.tcp.mutableValue()->port.setValue(32490);
     cfg.save(raw);
     beast->setConfig(raw);
 
